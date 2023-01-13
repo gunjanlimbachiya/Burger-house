af89b9804c79321bd14ca46ad915771d021a037f
Gunjan Limbachiya <Limbchiya.gunjan@investisdigital.com>
2023-01-13 13:14:32 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+
+    $(".events").slick({
+
+        slidesToShow:1,
+        slidesToScroll:1,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false
+
+       })
+    
+
+})
